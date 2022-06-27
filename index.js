0665f4f4f91cda4590ad11005b58667bdc8220c4
mrcollier91 <colliermatt91@gmail.com>
2022-06-27 09:50:19 -0500

    commit tree 2a763d4e260ea6fbf6a1b44c9940f6bef3fc568f

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i< array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j]=== complement)return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +15,10 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  need to work through number checking to see if any combination of 2 add up to target array 
+
+
+
 */
 
 /*
